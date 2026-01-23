7cafe91feb790583516df719f069f4639b073985
DYX <dyxzzz666@gmail.com>
2026-01-24 07:23:26 +0800

    basic setup

---
+++ src/main/java/com/dyx/crossrow/CrossRowApplication.java
@@ -0,0 +1,13 @@
+package com.dyx.crossrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossRowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrossRowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dyx/crossrow/CrossRowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dyx.crossrow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossRowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
