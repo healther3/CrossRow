26c3eb5f4391fa9459e757eaa7fa25dc3015dda8
DYX <dyxzzz666@gmail.com>
2026-01-27 15:54:21 -0500

    implement vector store configuration

---
+++ src/main/java/com/dyx/crossrow/config/VectorStoreConfiguration.java
@@ -0,0 +1,24 @@
+package com.dyx.crossrow.config;
+
+import com.dyx.crossrow.rag.CrossRowDocumentLoader;
+import jakarta.annotation.Resource;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class VectorStoreConfiguration {
+
+    @Resource
+    private CrossRowDocumentLoader crossRowDocumentLoader;
+
+    @Bean
+    VectorStore CrossRowVectorStore(EmbeddingModel embeddingModel) {
+        SimpleVectorStore simpleVectorStore= SimpleVectorStore.builder(embeddingModel)
+                .build();
+        simpleVectorStore.add(crossRowDocumentLoader.loadMarkDownFiles());
+        return simpleVectorStore;
+    }
+}
---
+++ src/test/java/com/dyx/crossrow/rag/CrossRowDocumentLoaderTest.java
@@ -0,0 +1,17 @@
+package com.dyx.crossrow.rag;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CrossRowDocumentLoaderTest {
+    @Resource
+    private CrossRowDocumentLoader crossRowDocumentLoader;
+    @Test
+    void loadMarkDownFiles() {
+        crossRowDocumentLoader.loadMarkDownFiles();
+    }
+}
