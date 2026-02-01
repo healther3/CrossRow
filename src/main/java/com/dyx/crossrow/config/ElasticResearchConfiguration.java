e15e65d61b13740fa04ff6533f7bd553554a99b7
DYX <dyxzzz666@gmail.com>
2026-02-01 17:33:15 -0500

    feat: add elastic research config and related methods: build base client->jackson json mapping->ES client->test,

--- src/main/java/com/dyx/crossrow/config/ElasticResearchConfiguration.java
+++ src/main/java/com/dyx/crossrow/config/ElasticResearchConfiguration.java
@@ -1,4 +1,75 @@
 package com.dyx.crossrow.config;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import com.dyx.crossrow.properties.ElasticsearchProperties;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+@EnableConfigurationProperties(ElasticsearchProperties.class)
 public class ElasticResearchConfiguration {
+    @Bean
+    public RestClient restClient(ElasticsearchProperties properties) {
+        HttpHost httpHost = new HttpHost(
+                properties.getHost(),
+                properties.getPort(),
+                properties.getProtocol()
+        );
+
+        // 建立客户端，创建底层 RestClient, APP layer
+        RestClientBuilder builder = RestClient.builder(httpHost);
+
+        builder.setRequestConfigCallback(requestConfigBuilder -> requestConfigBuilder
+                .setConnectionRequestTimeout(properties.getConnectionTimeout())
+                .setSocketTimeout(properties.getSocketTimeout())
+        );
+        return builder.build();
+    }
+
+    // 创建传输层 transport layer - JSON 序列化/反序列化 - 请求/响应的编解码
+    @Bean
+    public ElasticsearchTransport elasticsearchTransport(RestClient restClient) {
+        // jackson json 处理器 - 传输
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        //json 传输
+        return new RestClientTransport(
+                restClient,
+                new JacksonJsonpMapper(objectMapper)
+        );
+    }
+
+    // 创造 elastic search 客户接口
+    @Bean
+    public ElasticsearchClient elasticsearchClient(ElasticsearchTransport transport) {
+        return new ElasticsearchClient(transport);
+    }
+
+    // elastic search 客户端初始化检查
+    @Bean
+    public ApplicationRunner elasticSearchClientTest(ElasticsearchClient elasticsearchClient){
+        return args -> {
+            try {
+                boolean connected = elasticsearchClient.ping().value();
+                if (connected) {
+                    log.info("Elasticsearch 连接成功");
+                }
+            } catch (Exception e) {
+                log.error("Elasticsearch 连接失败: {}", e.getMessage());
+                // 根据需要决定是否抛出异常阻止启动
+            }
+        };
+
+    }
 }
